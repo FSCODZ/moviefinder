a99cb49ba0d53facb432a9c12f1701ee6185dabf
Filip Stojanovic <filip033@outlook.com>
2024-10-07 11:14:00 +0200

    added comedy page

--- moviefinder/app/comedi-movies/page.tsx
+++ moviefinder/app/comedi-movies/page.tsx
@@ -1,9 +1,11 @@
-export default function ComedyMovies() {
-    return (
-      <div>
-        <h1>Comedy Movies</h1>
-        <p>Here you will find the latest comedy movies!</p>
-      </div>
-    );
-  }
-  
+import ComediMovies from "../components/ComediMovies";
+
+const ComediMoviesPage = () => {
+  return (
+    <div>
+      <ComediMovies />
+    </div>
+  );
+};
+
+export default ComediMoviesPage;
